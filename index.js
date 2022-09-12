31a0e116bf7b2f9784cc498879fb8a3bc09ce49b
hongweihsu <hongweih@student.unimelb.edu.au>
2022-09-12 10:03:51 +1000

    change engin node 16.x and change response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>
-    {res.send({hi: 'listen'});});
+    {res.send({bye: 'buddy'});});
 
 // if Heroku define port then use it, otherwise 5000
 const PORT = process.env.PORT || 5000;
